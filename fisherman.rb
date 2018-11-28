2db0102b61b8c66337903a222529359154b35714
Ben Batschelet <bartleby84@gmail.com>
2018-11-28 14:18:19 -0600

    Update URLs and install step, cleanup Travis file

--- fisherman.rb
+++ fisherman.rb
@@ -2,17 +2,16 @@ class Fisherman < Formula
   desc "fish plugin manager"
   homepage "https://github.com/fisherman/fisherman"
 
-  url "https://raw.githubusercontent.com/fisherman/fisherman/2.13.0/fisher.fish"
-  sha256 "48e28860e33d8387595d0d3cced91ee64569579e88d4294b99c9d5e45d7daef9"
+  url "https://raw.githubusercontent.com/jorgebucaran/fisher/3.1.1/fisher.fish"
+  sha256 "1145140e8adfc3c73ffcfadaddbc9c80abe485cc663d9df38a565f114a83b399"
 
-  head "https://github.com/fisherman/fisherman.git"
+  head "https://github.com/jorgebucaran/fisher.git"
 
   depends_on "fish"
 
   def install
-    (share/"fish/vendor_functions.d/").install "fisher.fish"
-    File.write("fisher-completion.fish", "fisher --complete")
-    fish_completion.install "fisher-completion.fish" => "fisher.fish"
+    fish_function.install "fisher.fish"
+
     ohai "You may need to restart any open terminal sessions for changes to take effect."
   end
 end
